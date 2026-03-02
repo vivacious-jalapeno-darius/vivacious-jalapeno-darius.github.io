5951db1f40a37433aec715dbd7c2d22c2c84a6c0
Vivaan Jalla-Dhar <3602437@spsd.sk.ca>
2026-03-02 12:22:48 -0600

    Update sketch.js

--- Interactive_Scene/sketch.js
+++ Interactive_Scene/sketch.js
@@ -20,9 +20,11 @@ let r = 255;
 let g = 255;
 let b = 255;
 
+// ----- Player variables -----
 // Player size
 let playerWidth = 10;
 let playerHeight = 100;
+
 // Player speed
 let speed = 20;
 
@@ -32,6 +34,9 @@ let p1y = 0;
 
 // P2 variable
 let p2y = 0;
+
+// ----- UI -----
+let pause = false;
 
 
 // ------------------------- 1 TIME FUNCTION -------------------------
@@ -65,36 +70,39 @@ function createPlayers() {
   rect(p2x, p2y, playerWidth, playerHeight);
 }
 
-function playerMovement() {
-  // --------------------- P1 ---------------------
-  if (keyIsDown(87)) {  //w 
-    p1y -= speed;
-    if (p1y <= 0) {
-      p1y = 0
-    }
-  }
-  if (keyIsDown(83)) { //s 
-    p1y += speed;
-    if (p1y + playerHeight >= height) {
-      p1y = height - playerHeight;
-    }
-  }
-  
-  // --------------------- P2 ---------------------
-  if (keyIsDown(38)) { // UP_ARROW
-    p2y -= speed;
-    if (p2y <= 0) {
-      p2y = 0;
-    }
-  }
-  if (keyIsDown(40)){ // DOWN_ARROW
-    p2y += speed;
-    if (p2y + playerHeight >= height) {
-      p2y = height - playerHeight;
-    }
-  }
 
+function playerMovement() {
+  if (pause === false) {
+    // --------------------- P1 ---------------------
+    if (keyIsDown(87)) {  //w 
+      p1y -= speed;
+      if (p1y <= 0) {
+        p1y = 0
+      }
+    }
+    if (keyIsDown(83)) { //s 
+      p1y += speed;
+      if (p1y + playerHeight >= height) {
+        p1y = height - playerHeight;
+      }
+    }
+    
+    // --------------------- P2 ---------------------
+    if (keyIsDown(38)) { // UP_ARROW
+      p2y -= speed;
+      if (p2y <= 0) {
+        p2y = 0;
+      }
+    }
+    if (keyIsDown(40)){ // DOWN_ARROW
+      p2y += speed;
+      if (p2y + playerHeight >= height) {
+        p2y = height - playerHeight;
+      }
+    }
+  }
 }
+
 
 function ballMovement() {
   ballX += dx;
