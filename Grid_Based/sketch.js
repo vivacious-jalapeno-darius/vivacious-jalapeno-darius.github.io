e3260a792d4c12e4c220124451f28c27c8aec9bd
Vivaan Jalla-Dhar <3602437@spsd.sk.ca>
2026-03-25 09:52:30 -0600

    added variables

--- Grid_Based/sketch.js
+++ Grid_Based/sketch.js
@@ -10,16 +10,47 @@
 
 
 // -------------------- VARIABLES -------------------- \\
-let casinoRed = "#B30000";
+const TABLE_SQUARE_SIZE = 67;
 
+let tableLeftMargin;
+let tableRightMargin;
+let tableTopMargin;
+let tableBottomMargin;
+
+let tableRows;
+let tableCols;
+let casinoRed = "#B30000";
 
 
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(casinoRed);
+  tableRows = Math.floor(width/TABLE_SQUARE_SIZE);
+  tableCols = Math.floor(height/TABLE_SQUARE_SIZE);
+  makeTable(tableRows, tableCols);
 }
 
-function draw() {
-  background(casinoRed);
+
+function makeTable(tableRows, tableCols) {
+  tableLeftMargin = width/10;
+  tableTopMargin = height/10;
+  tableRightMargin = width-tableLeftMargin;
+  tableBottomMargin = height-tableTopMargin
+
+
+  for (let y = tableTopMargin; y < tableBottomMargin; y++){
+    
+  }
 }
+
+
+function draw() {
+  
+  
+}
+
+
+
+
