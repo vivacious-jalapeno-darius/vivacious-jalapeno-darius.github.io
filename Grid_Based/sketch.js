f1348ea87f0b0927f95bc170f88340d0c973508e
Vivaan Jalla-Dhar <3602437@spsd.sk.ca>
2026-03-26 22:38:18 -0600

    made a start screen and imported some fonts (no start button made yet)

--- Grid_Based/sketch.js
+++ Grid_Based/sketch.js
@@ -10,7 +10,7 @@
 
 
 // ------------------------- VARIABLES ------------------------- \\
-const TABLE_SQUARE_SIZE = 101;
+const TABLE_SQUARE_SIZE = 135;
 
 let margin = TABLE_SQUARE_SIZE / 2;
 
@@ -23,13 +23,16 @@ let ypos;
 let mysteryBox;
 
 let titleSize;
-let titleText = "";
+let titleText = "BIG DON'S CASINO";
+let subTitleText = "All $$$ goes straight to Vivaan Jalla-Dhar (no refunds)";
+let font;
+
 // ----- COLOURS -----
 let casinoRedBackground = "#B30000";
 let casinoGoldTable = "#EFBF04";
 let textColour = "black";
 // ----- GAME STATUS -----
-let gameStatus = "gambling";
+let gameStatus = "start";
 // ------------------------------------------------------------- \\
 
 
@@ -38,13 +41,22 @@ let gameStatus = "gambling";
 // ------------------------- 1 TIME FUNCTIONS ------------------------ \\
 function preload(){
   mysteryBox = loadImage('mystery_box.png');
+  font = loadFont("AmericanCaptain-MdEY.otf");
 }
 
 
 
 function setup() {
-  titleSize = (width + height) / 10;
+  titleSize = (width + height)* 2/3;
+  subTitleSize = (width + height) / 20;
   createCanvas(windowWidth, windowHeight);
+}
+
+
+
+
+// ------------------------- LOOPING FUNCTIONS -------------------------\\
+function draw() {
   background(casinoRedBackground);
   if (gameStatus === "start") {
     startScreen();
@@ -56,6 +68,7 @@ function setup() {
     makeTable();
   }
 }
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \\
 
 
 
@@ -63,7 +76,10 @@ function startScreen() {
   textAlign(CENTER, CENTER);
   textSize(titleSize);
   fill(textColour);
-  text();
+  textFont(font);
+  text(titleText, width/2, height * (2/5));
+  textSize(subTitleSize);
+  text(subTitleText, width/2, height * (3/5));
 }
 
 
@@ -87,15 +103,12 @@ function makeTable() {
 
 
 
+
 function mathFlooring() {
   tableCols = Math.floor((width - margin * 2) / TABLE_SQUARE_SIZE);
   tableRows = Math.floor((height - margin * 2) / TABLE_SQUARE_SIZE);
 }
 
-// ------------------------- LOOPING FUNCTIONS -------------------------\\
-function draw() {
-  
-}
 
 
 
